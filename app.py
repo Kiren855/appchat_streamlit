ac626b8e4ff3ae9e3fd494c37fc2475375ec271e
Nong Ngoc Huan <95275091+Kiren855@users.noreply.github.com>
2024-11-15 10:16:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,12 +18,14 @@ class Block:
         return sha.hexdigest()
 
 # Blockchain structure
+# Cấu trúc Blockchain
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
 
     def create_genesis_block(self):
-        return Block(0, str(datetime.utcnow()), "Genesis Block", "0")
+        # Dùng dictionary để đồng bộ dữ liệu
+        return Block(0, str(datetime.utcnow()), {"user": "Genesis", "message": "Genesis Block"}, "0")
 
     def get_latest_block(self):
         return self.chain[-1]
@@ -32,6 +34,7 @@ class Blockchain:
         latest_block = self.get_latest_block()
         new_block = Block(len(self.chain), str(datetime.utcnow()), data, latest_block.hash)
         self.chain.append(new_block)
+
 
 # Initialize blockchain
 blockchain = Blockchain()
